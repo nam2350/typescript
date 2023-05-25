8600fc05149fab670316be7722d6a39810cbbc2d
Nam Jae Hyun <nam2350@gmail.com>
2023-05-25 19:50:30 +0900

    추상화

--- 3-oop/3-4-abstraction.ts
+++ 3-oop/3-4-abstraction.ts
@@ -1,3 +1,75 @@
 {
-  
+    type CoffeeCup = {
+        shots: number;
+        hasMilk: boolean;
+    }
+
+    class CoffeeMaker {
+        public static BEANS_GRAMM_PER_SHOT: number = 7; // class level
+        private coffeeBeans: number = 0;
+
+        private constructor(coffeeBeans: number) {
+            this.coffeeBeans = coffeeBeans
+        }
+
+        static makeMachine(coffeeBeans: number): CoffeeMaker {
+            return new CoffeeMaker(coffeeBeans)
+        }
+
+        fillCoffeeBeans(beans: number) {
+            if (beans < 0) {
+                throw new Error("value for beans should be greater than 0");
+            }
+            this.coffeeBeans += beans;
+        }
+
+        grindBeans(shots) {
+            console.log(`grinding beans for ${shots}`);
+            if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+                throw new Error('Not enough coffee beans!');
+            }
+            this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+        }
+        public makeCoffee(shots: number): CoffeeCup {
+            this.grindBeans(shots);
+            this.preheat();
+            return this.extract(shots);
+            // if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
+            //     throw new Error('Not enough coffee beans!');
+            // }
+            // this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+            // return {
+            //     shots: shots,
+            //     hasMilk: false
+            // };
+        }
+    }
+
+    const maker = CoffeeMaker.makeMachine(30);
+    maker.fillCoffeeBeans(20);
+
+    class User {
+        get fullName(): string {
+            return `${this.firstName} ${this.lastName}`;
+        }
+        private internalAge = 4;
+        get age(): number {
+            return this.internalAge;
+        }
+        set age(num: number) {
+            if (num <= 0) {
+                throw new Error("Invalid value, age should be grater than 0")
+
+            }
+            this.internalAge = num;
+        }
+        constructor(private firstName: string, private lastName: string) {
+        }
+    }
+
+    const user = new User('Steve', 'Jobs');
+    console.log(user.age)
+    user.age = 6
+    console.log(user.age)
+
 }
